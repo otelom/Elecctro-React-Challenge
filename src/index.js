8d74d9a96dcc8ee9e0a022caecced2c4ba4c6165
Otelo Magalhães <otelom@users.noreply.github.com>
2018-03-23 00:03:29 +0000

    Added Local Storage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {Provider} from "react-redux";
-import { createStore } from 'redux';
+//import { createStore } from 'redux';
 import reducer from "./reducers/reducers";
 //import reducer from "./reducers/mainReducer";
 
+import * as storage from 'redux-storage'
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import createEngine from 'redux-storage-engine-localstorage';
+//const reducer = storage.reducer(combineReducers(reducers));
+const engine = createEngine('my-save-key');
+const middleware = storage.createMiddleware(engine);
+const createStoreWithMiddleware = applyMiddleware(middleware)(createStore);
+const store = createStoreWithMiddleware(reducer);
+const load = storage.createLoader(engine);
+load(store)
+    .then((newState) => console.log('Loaded state:', newState))
+    .catch(() => console.log('Failed to load previous state'));
 
-let store = createStore(reducer)
+
+//let store = createStore(reducer)
 //let store = createStore(todoApp, window.STATE_FROM_SERVER)
 ReactDOM.render(
     <Provider store={store}>
