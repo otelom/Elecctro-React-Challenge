c88dcddab62cbbe90a6b14aee38db4ff6b056dc4
Otelo Magalhães <otelom@users.noreply.github.com>
2018-03-24 06:11:02 +0000

    Close Edit Mode on Click Outside

--- src/components/TODO.js
+++ src/components/TODO.js
@@ -13,6 +13,28 @@ class TODO extends Component {
             text: props.text,
             class: props.state === COMPLETE ? 'checked item' : 'item'
         };
+        this.setWrapperRef = this.setWrapperRef.bind(this);
+        this.handleClickOutside = this.handleClickOutside.bind(this);
+    }
+
+    componentDidMount() {
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside);
+    }
+
+    setWrapperRef(node) {
+        this.wrapperRef = node;
+    }
+    handleClickOutside(event) {
+        if (this.state.editMode && this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+            if (this.state.text !== '') {
+                this.props.editTODO(this.props.id, this.state.text);
+                this.setState({editMode: false});
+            }
+        }
     }
 
     _handleKeyPress = function(e) {
@@ -55,7 +77,9 @@ class TODO extends Component {
         return (
             <li className={this.state.class} key={this.props.id} onClick={() => this.setState({editMode: true})}>
                 <span className="check" onClick={(event) => this.handleStatusChange(event)}>✓</span>
-                <span className="text"> {this.checkEditMode()} </span>
+                <div ref={this.setWrapperRef}>
+                    <span className="itemText"> {this.checkEditMode()} </span>
+                </div>
                 <span className="delete" onClick={(event) => {this.props.remove(this.props.id); event.stopPropagation();}}>×</span>
             </li>
         );
@@ -68,5 +92,6 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
     check: (id) => dispatch(check(id)),
     uncheck: (id) => dispatch(uncheck(id))
 });
+
 
 export default connect(null, mapDispatchToProps)(TODO);
